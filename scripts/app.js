433b343371cb951356028d1e905e996393b02217
kevin deutscher <kevin.m.deutscher@gmail.com>
2015-12-06 12:09:44 -0600

    date field added

--- scripts/app.js
+++ scripts/app.js
@@ -5,21 +5,29 @@ $(document).ready(function(){
 
 });//end of document.ready
 
-var inputDate = '2015-12-04T20:00:00';
-var inputZipCode = '60618';
+var inputDate = '2015-12-06';
+var testDate;
 var userInputZip;
+var userInputDate;
 
 function runAjax(){
   $.ajax(
     {type: 'get',
     url:
       'http://api.jambase.com/events?zipCode='+userInputZip+'&api_key=yhr4bp7wwbq722r2a6bwef2w',
+      // 'http://api.jambase.com/events?zipCode='+userInputZip+'&api_key=uzhe5fzcruqrewep5cj2jrjz',
+      // uzhe5fzcruqrewep5cj2jrjz
       success: function (data){
-        for (i=0; i< 10; i++){
-          if(data.Events[i].Date == inputDate){
+        console.log(data.Events.length);
+        console.log(data);
+
+        for (i=0; i< data.Events.length; i++){
           var shortDate = data.Events[i].Date.substring(0,10);
           var shortTime = data.Events[i].Date.substring(11,19);
-          // dataArr.push(data.Events[i].Date, data.Events[i].Artists[0].Name);
+          console.log(shortDate);
+          console.log(userInputDate);
+
+          if(shortDate == userInputDate){
           $("#divID").append('</p>' + 'date::: '+ shortDate + ' time:::' + shortTime + ' show::: ' + data.Events[i].Artists[0].Name + ' address::: ' + data.Events[i].Venue.Address + ' city::: ' + data.Events[i].Venue.City + '</p>');
         }
       }
@@ -31,10 +39,17 @@ function runAjax(){
   );
 }
 
+
+function convertUserDate(value){
+    var arr = value.split("-").reverse().join('-');
+    return arr;
+}
+
 function findShows(){
   var userZip = document.getElementById("userInput");
+  var userDate = document.getElementById("userDate");
   userInputZip = userZip.value;
-  document.getElementById("divID").innerHTML = userInputZip;
+  userInputDate = convertUserDate(userDate.value);
   runAjax();
 }
 ////////////////////
